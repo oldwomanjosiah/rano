ec9656e28b9475507fb157acddd502030dd1ab47
Josiah Hilden <jhilden13@gmail.com>
2021-03-24 00:09:05 -0500

    Add either module and enum for non-result sum types

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,53 @@
 pub mod ass2;
 
 // pub use ass::assemble_str as assemble_stripped;
+
+pub mod either {
+
+    pub use Either::*;
+    pub enum Either<T, U> {
+        L(T),
+        R(U),
+    }
+
+    impl<T, U> From<Either<T, U>> for Result<T, U> {
+        fn from(e: Either<T, U>) -> Result<T, U> {
+            match e {
+                L(t) => Ok(t),
+                R(u) => Err(u),
+            }
+        }
+    }
+
+    impl<T, U> Either<T, U> {
+        pub fn left(self) -> Option<T> {
+            if let L(t) = self {
+                Some(t)
+            } else {
+                None
+            }
+        }
+
+        pub fn right(self) -> Option<U> {
+            if let R(t) = self {
+                Some(t)
+            } else {
+                None
+            }
+        }
+
+        pub fn swap(self) -> Either<U, T> {
+            match self {
+                L(t) => R(t),
+                R(u) => L(u),
+            }
+        }
+
+        pub fn as_ref(&self) -> Either<&T, &U> {
+            match self {
+                L(t) => L(t),
+                R(u) => R(u),
+            }
+        }
+    }
+}
